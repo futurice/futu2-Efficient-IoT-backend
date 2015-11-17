e5539a0b3f7f8e665d363d17c8c3fdad257638a9
minevala <mika.nevalainen@futurice.com>
2015-11-17 13:31:28 +0200

    Add JS code style linter

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
 #!/usr/bin/env node
-const WebSocketServer = require('websocket').server;
-const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const routes = require('./routes/index');
 
-const server = http.createServer((request, response) => {
-    console.log(`${new Date()} Received request for ${request.url}`);
-    response.writeHead(404);
-    response.end();
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', routes);
+app.use((err, req, res, next) => {
+  logger('error:' + err.message);
+});
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-server.listen(8080, console.log(`${new Date()} Server is listening on port 8080`));
+app.io = socketIO();
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
+app.io.on('connection', socket => {
+  console.log('user connected');
 });
 
-const originIsAllowed = origin => { return true };
+app.io.on('connection', socket => {
 
-wsServer.on('request', request => {
+  socket.on('user', msg => {
+    console.log('user', msg);
+    app.io.emit('user', msg);
+  });
 
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((`${new Date()} Connection from origin ${request.origin} rejected.`));
-      return;
-    }
+  socket.on('new message', msg => {
+    console.log('new message:', msg);
+    app.io.emit('new message', msg);
+  });
 
-    const connection = request.accept('echo-protocol', request.origin);
-    console.log(`{new Date()} Connection accepted.`);
-    connection.on('message', message => {
-        if (message.type === 'utf8') {
-            console.log(`Received Message: ${message.utf8Data}`);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log(`Received Binary Message of ${message.binaryData.length} bytes`);
-            connection.sendBytes(message.binaryData);
-        }
-    });
-    connection.on('close', (reasonCode, description) => {
-        console.log(`${new Date()} Peer ${connection.remoteAddress} disconnected.`);
-    });
+  socket.on('disconnect', () => console.log('user disconnected'));
 });
+
+module.exports = app;
+
