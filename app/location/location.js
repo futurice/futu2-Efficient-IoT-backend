18b67bef767ec382a8d05453fdf18aa195d2629f
minevala <mika.nevalainen@futurice.com>
2015-11-19 13:51:08 +0200

    Fix streams handling

--- app/location/location.js
+++ app/location/location.js
@@ -1,19 +1,22 @@
+const Rx = require('rx');
 const { beacons } = require('../../config/config');
 
-const findIndexById = (arr, id) => arr.findIndex(x => x && x.id === id);
 const isValidNumber = (x) => !(isNaN(x) || x + x === x);
-const curryCalculate = calculate([]);
 
-exports.fromDeviceStream = function listen(stream) {
-  return stream
-    .map(mapDeviceLocation)
-    .map(curryCalculate)
+exports.fromDeviceStream = stream => {
+  const beaconStreams = splitInToBeaconStreams(stream, beacons);
+  return Rx.Observable.combineLatest(beaconStreams, (...results) => calculatePosition(results));
 };
 
-function mapDeviceLocation(device) {
-  const index = findIndexById(beacons, device.id);
-  const beacon = beacons[index];
+function splitInToBeaconStreams(stream, beacons) {
+  return beacons.map(beacon => {
+      return stream
+        .filter(s => s.id === beacon.id)
+        .map(device => mapDeviceLocation(device, beacon));
+    });
+}
 
+function mapDeviceLocation(device, beacon) {
   return {
     id: device.id,
     distance: device.distance,
@@ -26,43 +29,17 @@ function mapDeviceLocation(device) {
  http://everything2.com/title/Triangulate
  http://stackoverflow.com/questions/20332856/triangulate-example-for-ibeacons#answer-20976803
  */
-function calculate(initialObjects) {
-  const DEFAULT = {
-    x: 0,
-    y: 0
-  };
+function calculatePosition(devices) {
 
-  var arr = initialObjects;
-
-  return function (device) {
-    arr =
-      arr
-        .filter(obj => obj.id !== device.id)
-        .concat([device])
-        .slice(-3);
-
-    if (arr.length < 3) {
-      return DEFAULT;
-    }
-
-    const { x, y } = calculatePosition(arr, DEFAULT);
-
-    return {
-      x: isValidNumber(x) && x || 0,
-      y: isValidNumber(y) && y || 0
-    };
-  }
-}
-
-function calculatePosition(objects){
-  let [obj, obj2, obj3] = objects;
+  let [obj, obj2, obj3, ...rest] = devices;
   const W = Math.pow(obj.distance, 2) - Math.pow(obj2.distance, 2) - Math.pow(obj.x, 2) - Math.pow(obj.y, 2) + Math.pow(obj2.x, 2) + Math.pow(obj2.y, 2);
   const Z = Math.pow(obj2.distance, 2) - Math.pow(obj3.distance, 2) - Math.pow(obj2.x, 2) - Math.pow(obj2.y, 2) + Math.pow(obj3.x, 2) + Math.pow(obj3.y, 2);
   const x = (W * (obj3.y - obj2.y) - Z * (obj2.y - obj.y)) / (2 * ((obj2.x - obj.x) * (obj3.y - obj2.y) - (obj3.x - obj2.x) * (obj2.y - obj.y)));
   const y = (W - 2 * x * (obj2.x - obj.x)) / (2 * (obj2.y - obj.y));
 
   return {
-    x: x,
-    y: y
+    x: isValidNumber(x) && x || 0,
+    y: isValidNumber(y) && y || 0
   };
 }
+
