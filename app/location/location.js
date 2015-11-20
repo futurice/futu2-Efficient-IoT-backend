f7077d48ce9e887af41501f80ef17eb27a7ad838
minevala <mika.nevalainen@futurice.com>
2015-11-20 13:59:54 +0200

    Fix build

--- app/location/location.js
+++ app/location/location.js
@@ -1,8 +1,6 @@
 const Rx = require('rx');
 const { beacons } = require('../../config/config');
 
-const isValidNumber = (x) => !(isNaN(x) || x + x === x);
-
 exports.fromDeviceStream = stream => {
   const beaconStreams = splitInToBeaconStreams(stream, beacons);
   return Rx.Observable.combineLatest(beaconStreams, (...results) => calculatePosition(results));
@@ -10,8 +8,10 @@ exports.fromDeviceStream = stream => {
 
 function splitInToBeaconStreams(stream, beacons) {
   return beacons.map(beacon => {
+
+
       return stream
-        .filter(s => !(s.floor) || s.floor === beacon.floor) // FIXME: remove !(s.floor) condition. Used before we had floor in beacon event.
+        .filter(s => !(s.floor) || s.floor === beacon.floor) // FIXME: remove !(s.floor) condition.
         .filter(s => s.id === beacon.id)
         .map(device => mapDeviceLocation(device, beacon));
     });
@@ -30,21 +30,25 @@ function mapDeviceLocation(device, beacon) {
  http://everything2.com/title/Triangulate
  http://stackoverflow.com/questions/20332856/triangulate-example-for-ibeacons#answer-20976803
  */
+
 function calculatePosition(devices) {
 
   let [obj, obj2, obj3, ...rest] = devices;
   const W = getIntersectionPoint(obj, obj2);
   const Z = getIntersectionPoint(obj2, obj3);
-  const x = (W * (obj3.y - obj2.y) - Z * (obj2.y - obj.y)) / (2 * ((obj2.x - obj.x) * (obj3.y - obj2.y) - (obj3.x - obj2.x) * (obj2.y - obj.y)));
+  const Y = (W * (obj3.y - obj2.y) - Z * (obj2.y - obj.y));
+  const x = Y / (2 * ((obj2.x - obj.x) * (obj3.y - obj2.y) - (obj3.x - obj2.x) * (obj2.y - obj.y)));
   const y = (W - 2 * x * (obj2.x - obj.x)) / (2 * (obj2.y - obj.y));
 
   return {
-    x: isValidNumber(x) && x || 0,
-    y: isValidNumber(y) && y || 0
+    x: isValidPosition(x) && x || 0,
+    y: isValidPosition(y) && y || 0
   };
 }
-
 const getIntersectionPoint = (obj1, obj2) => {
-  return Math.pow(obj1.distance, 2) - Math.pow(obj2.distance, 2) - Math.pow(obj1.x, 2) - Math.pow(obj1.y, 2) + Math.pow(obj2.x, 2) + Math.pow(obj2.y, 2)
+  const areaDifference = Math.pow(obj1.distance, 2) - Math.pow(obj2.distance, 2);
+  return areaDifference - Math.pow(obj1.x, 2) - Math.pow(obj1.y, 2) + Math.pow(obj2.x, 2) + Math.pow(obj2.y, 2);
 };
+
+const isValidPosition = (x) => !(isNaN(x) || x + x === x); // infinity + infinity = infinity
 
