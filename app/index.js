6375885f792eecb244e42a41ee984c4f159a01ff
minevala <mika.nevalainen@futurice.com>
2015-11-23 18:06:29 +0200

    Improve test page layout

--- app/index.js
+++ app/index.js
@@ -13,6 +13,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
