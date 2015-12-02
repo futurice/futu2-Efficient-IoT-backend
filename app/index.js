c8c12079ad07492268388b4bd366c395f8bef849
minevala <mika.nevalainen@futurice.com>
2015-12-02 15:08:49 +0200

    Separate views to separate folder

--- app/index.js
+++ app/index.js
@@ -2,39 +2,21 @@
 const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
-const logger = require('morgan');
 const Rx = require('rx');
-const bodyParser = require('body-parser');
 const Storage = require('app/storage');
-const routes = require('app/routes');
 const location = require('app/location');
+const views = require('app/views');
 
-// init app
+// init app setup
 const app = express();
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, './views/js')));
+// render test page
+views.renderTestPage(app);
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/', routes);
-app.use((err) => {
-  logger('error:' + err.message);
-});
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// init storage
-const appStorage = new Storage();
+// init cache storage
+const appCache = new Storage();
 
 
 // set up socket.IO
@@ -57,7 +39,7 @@ location.fromDeviceStream(beaconSource)
 
 // set data
 const messageSource = connectionSource.flatMap(observableFromSocketEvent('message'));
-const setStorageSource = messageSource.flatMap(message => appStorage.set(message));
+const setStorageSource = messageSource.flatMap(message => appCache.set(message));
 
 setStorageSource
   .subscribe(
@@ -69,7 +51,7 @@ setStorageSource
 const initSource = connectionSource.flatMap(observableFromSocketEvent('init'));
 const storageSource =
   initSource
-    .flatMap(val => appStorage.getAll());
+    .flatMap(val => appCache.getAll());
 
 storageSource
     .subscribe(
